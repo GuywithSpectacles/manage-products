21932f9c6f4bfee726f3622edec74f8adcfbcd54
Aqeel Ahmad <aqeel4622@outlook.com>
2023-10-19 17:41:04 +0500

    CRUD operations for Products have been established

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+} from '@nestjs/common';
+import { CreateProductDTO } from './dto/create-product.dto';
 
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  @Get()
+  findAll(): string {
+    return 'These are all Products';
+  }
+
+  @Get()
+  findOne(@Param('id') id): string {
+    return `This is a string with the id: ${id}`;
+  }
+
+  @Post()
+  create(@Body() createProductDTO: CreateProductDTO): string {
+    return `
+    Name: ${createProductDTO.genericName}
+    Brand: ${createProductDTO.brand}
+    Code: ${createProductDTO.code}
+    Group: ${createProductDTO.group}
+    Cost: ${createProductDTO.cost}
+    Price: ${createProductDTO.price}
+    UnitOfMearure: ${createProductDTO.unitOfMearure}
+    PackingType: ${createProductDTO.packingType}
+    barCode: ${createProductDTO.barCode}
+    `;
+  }
+
+  @Delete()
+  delete(@Param('id') id): string {
+    return `Product is deleted ${id}`;
+  }
+
+  @Put()
+  update(@Body() createProductDTO: CreateProductDTO, @Param('id') id): string {
+    return `
+    Update ${id} -
+    Name: ${createProductDTO.genericName}
+    Brand: ${createProductDTO.brand}
+    Code: ${createProductDTO.code}
+    Group: ${createProductDTO.group}
+    Cost: ${createProductDTO.cost}
+    Price: ${createProductDTO.price}
+    UnitOfMearure: ${createProductDTO.unitOfMearure}
+    PackingType: ${createProductDTO.packingType}
+    barCode: ${createProductDTO.barCode}
+    `;
+  }
+}
