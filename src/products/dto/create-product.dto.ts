d656f0f56450b64729515083d7641b79df852b18
Aqeel Ahmad <aqeel4622@outlook.com>
2023-10-19 16:29:56 +0500

    products controller and DTO established

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,11 @@
+export class CreateProductDTO {
+  readonly genericName: string;
+  readonly brand: string;
+  readonly code: number;
+  readonly group: string;
+  readonly cost: number;
+  readonly price: number;
+  readonly unitOfMearure: string;
+  readonly packingType: string;
+  readonly barCode: string;
+}
